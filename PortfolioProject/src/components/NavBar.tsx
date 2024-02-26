ad659a23c238b8539d51e579a65e2e921bd42521
Peter Brohnst <126153672+Peter8790@users.noreply.github.com>
2024-02-26 13:37:05 +0100

    boiler setup + tailwind

---
+++ PortfolioProject/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+
+
+export default function NavBar() {
+    return (
+        <nav>
+        <h1>Portfolio</h1>
+        <div className="links">
+            <a href="/">Test</a>
+            <a href="/create">Test</a>
+        </div>
+        </nav>
+    )
+    }
